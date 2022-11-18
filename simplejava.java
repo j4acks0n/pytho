46b8de23f19841c50fa9fbc1bfa35f938ccfac84
j4acks0n <116141186+j4acks0n@users.noreply.github.com>
2022-11-18 22:55:59 +0700

    Add files via upload

---
+++ simplejava.java
@@ -0,0 +1,10 @@
+public class simplejava  
+{  
+public static void main(String...k)  
+{  
+int num1=80-16;  
+double num2=10-9.6;  
+System.out.println("num1: "+num1);  
+System.out.println("num2: "+num2);  
+}  
+}  
---
+++ simplemath.py
@@ -0,0 +1,8 @@
+import math
+
+pi = 1.20*8
+a = 8+8
+b = 9+9
+print(a);
+print(pi);
+print(a+b)
